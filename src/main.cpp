8f4c30f0ee73ca1f531e089d02d09ea032a86612
YasiruRF <dyrfernando@gmail.com>
2026-09-17 15:29:24 +0530

    removed bluetooth/ motors/ voltage divider

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,6 @@
 
 #include "sensor.h"
 #include "encoder.h"
-#include "bluetooth.h"
-#include "battery.h"
-#include "motor.h"
 
 void setup()
 {
@@ -12,13 +9,6 @@ void setup()
 
     sensor_init();
     encoder_setup();
-    battery_setup();
-    motor_setup();
-
-    // bluetooth_init();
-
-    // Spin both motors forward
-    drive(150, 150);
 }
 
 void loop()
@@ -26,37 +16,15 @@ void loop()
     sensor_reading_t reading;
     sensor_read_all(&reading);
 
-    bluetooth_handle();
-
-    float battery = battery_voltage();
-
     Serial.printf(
-        "front=%u left=%u right=%u | L=%ld R=%ld | D=%.2f cm | V=%.2f V\n",
+        "front=%u left=%u right=%u | L=%ld R=%ld | D=%.2f cm\n",
         reading.front_mm,
         reading.left_mm,
         reading.right_mm,
         left_ticks,
         right_ticks,
-        GetDistanceTraveled(),
-        battery
+        GetDistanceTraveled()
     );
-
-    // char data[120];
-
-    // snprintf(
-    //     data,
-    //     sizeof(data),
-    //     "S %u %u %u E %ld %ld D %.2f V %.2f",
-    //     reading.front_mm,
-    //     reading.left_mm,
-    //     reading.right_mm,
-    //     left_ticks,
-    //     right_ticks,
-    //     GetDistanceTraveled(),
-    //     battery
-    // );
-
-    // bluetooth_send(data);
 
     delay(50);
 }
